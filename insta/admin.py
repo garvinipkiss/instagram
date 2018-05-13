c3fd2f466929c87c3a2c0774fd895ab41c21a7c1
garvinipkiss <garvinipkiss@gmail.com>
2018-05-13 14:40:45 +0300

    registering admin

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Profile, Comment, Like, Photo
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Photo)
